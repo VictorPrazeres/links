98c9cfb5a4425fc045f58fbfaaeaa5e3dda8a5e4
Victor Prazeres <dev.vprazeres@gmail.com>
2025-12-28 13:50:20 -0300

    feat: add modal component to Index view for displaying link details

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,12 @@
 import { MaterialIcons } from "@expo/vector-icons"
-import { FlatList, Image, TouchableOpacity, View } from "react-native"
+import {
+  FlatList,
+  Image,
+  Modal,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native"
 
 import { colors } from "@/styles/colors"
 import { styles } from "./styles"
@@ -34,6 +41,28 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modalLinkName}>Rocketseat</Text>
+
+            <Text style={styles.modalUrl}>https://www.rocketseat.com.br</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
